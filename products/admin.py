bf0a2eb5bb4432584b050317a964909827b4b44c
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-03-29 16:33:03 +0000

    feat: create models in products app, register models in products admin, migrate models to database and create fixtures folder

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ProductCategory, YarnCategory, ToolCategory, YarnBrand, ToolBrand, Product
 
 # Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(YarnCategory)
+admin.site.register(ToolCategory)
+admin.site.register(YarnBrand)
+admin.site.register(ToolBrand)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+
+class YarnCategory(models.Model):
+    yarn_category_name = models.CharField(max_length=200)
+    yarn_friendly_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.yarn_category_name
+    
+    def get_friendly_name(self):
+        return self.yarn_friendly_name
+
+
+class ToolCategory(models.Model):
+    tool_category_name = models.CharField(max_length=200)
+    tool_friendly_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.tool_category_name
+    
+    def get_friendly_name(self):
+        return self.tool_friendly_name
+
+
+class YarnBrand(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class ToolBrand(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    BLACK = "Black"
+    GREY = "Grey"
+    WHITE = "White"
+    RED = "Red" 
+    BLUE = "Blue"
+    GREEN = "Green"
+    YELLOW = "Yellow"
+    ORANGE = "Orange"
+    PINK = "Pink"
+    PURPLE = "Purple"
+    BROWN = "Brown"
+    MULTI = "Multi"
+    COLOR_CHOICES = [
+        (BLACK, "Black"),
+        (GREY, "Grey"),
+        (WHITE, "White"),
+        (RED, "Red"),
+        (BLUE, "Blue"),
+        (GREEN, "Green"),
+        (YELLOW, "Yellow"),
+        (ORANGE, "Orange"),
+        (PINK, "Pink"),
+        (PURPLE, "Purple"),
+        (BROWN, "Brown"),
+        (MULTI, "Multi")
+    ]
+    product_category = models.ForeignKey('ProductCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    yarn_category = models.ForeignKey('YarnCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    tool_category = models.ForeignKey('ToolCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES, default=MULTI)
+    yarn_brand = models.ForeignKey('YarnBrand', null=True, blank=True, on_delete=models.SET_NULL)
+    tool_brand = models.ForeignKey('ToolBrand', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
