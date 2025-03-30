a005cfbc82beff9b243b671f5bf75f597dfd1690
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-03-30 18:14:15 +0100

    feat: create view and url for all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """
+    A view to show all the products
+    """
+    products_all = Product.objects.all()
+
+    return render(
+        request,
+        'products/products.html',
+        {
+            "products_all": products_all,
+        }
+    )
