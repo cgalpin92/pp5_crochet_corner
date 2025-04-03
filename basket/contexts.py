7c4c1f6d3e4e3c90b53739b1ad194d879fbd4ca4
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-03 15:40:09 +0100

    feat: add to context.py to update shopping basket and grand total with products added

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quatity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quatity * product.price
+        product_count += quatity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quatity,
+            'product': product,
+        })
+
 
     if total < settings.FREE_DELIVERY_LIMIT:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
