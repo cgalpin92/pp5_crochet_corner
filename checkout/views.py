b2e44c241fe6a83e1904a12523124a610481543a
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-05 12:50:46 +0100

    feat: add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
         request,
         'checkout/checkout.html',
         {
-            "order_form": order_form
+            "order_form": order_form,
+            "stripe_public_key": "pk_test_51RAURpQPBrj6DHsXp9CnpeudhabhiuZpywrN3fWFX0JGLAtKD8IJy5CrAqzIzxsnAh3sBzhwmxWw30tFjBtyJHAz00mXXwDLJH",
+            "client_secret": "test client secret",
         }
     )
