d0d5c5cfbcbfaa8eb768b08d29a5310cab2d85f9
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-05 17:48:49 +0100

    feat: update checkout view to handle post method in payment submit

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from basket.contexts import basket_contents
+
 import stripe
 
 
@@ -10,33 +14,97 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "There is nothing in your basket \
-                       at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
+
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "There was an issue with one of the items in your" \
+                        "basket, please get in touch so we can get this" \
+                        "resolved for you"
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                           Please double check your information.')
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There is nothing in your basket \
+                        at the moment")
+            return redirect(reverse('products'))
+
+        current_basket = basket_contents(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, "Stripe public key is missing. \
+                            Did you forget to set it in your environment?")
+
+        return render(
+            request,
+            'checkout/checkout.html',
+            {
+                "order_form": order_form,
+                "stripe_public_key": stripe_public_key,
+                "client_secret": intent.client_secret,
+            }
+        )
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    Taken from Boutique Ado walkthrough
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+                    Your order number is {order_number}. A confirmation \
+                    email will be sent to {order.email}.')
     
-    current_basket = basket_contents(request)
-    total = current_basket['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-
-    order_form = OrderForm()
-
-    if not stripe_public_key:
-        messages.warning(request, "Stripe public key is missing. \
-                         Did you forget to set it in your environment?")
+    if 'basket' in request.session:
+        del request.session['basket']
 
     return render(
         request,
-        'checkout/checkout.html',
+        'checkout/checkout_success.html',
         {
-            "order_form": order_form,
-            "stripe_public_key": stripe_public_key,
-            "client_secret": intent.client_secret,
+            "order": order,
         }
     )
