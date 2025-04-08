83b84ddd2be1c15839d2bed1162e3774853ce7bd
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-09 00:19:17 +0100

    feat: create user profile form and render this to profile.html. create order_history.html, order_history view and order_history urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
 
     return render(
         request,
         'profiles/profile.html',
         {
-            "profile": profile
+            "form": form,
+        }
+    )
+
+
+def order_history(request):
+    """
+    Displays the users order history
+    """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    return render(
+        request,
+        'profiles/order_history.html',
+        {
+            "orders": orders
         }
     )
