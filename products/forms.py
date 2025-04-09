c27ea3909e934cf44c161d860d52f2a5b389a5e7
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-09 22:40:17 +0100

    feat: edit image field for editing and adding new products

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, ProductCategory, ToolCategory, YarnCategory, ToolBrand, YarnBrand
 
 
@@ -7,3 +8,5 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
