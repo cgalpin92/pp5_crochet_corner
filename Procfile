03bfe13e01faca3655b8f878b0e61c3a212c7ce9
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-11 17:18:09 +0100

    prepare project for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crochet_corner.wsgi:application
--- crochet_corner/settings.py
+++ crochet_corner/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.isfile('env.py'):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5_=x-(z%x+ntod7h!$9=!us-6i-g+l*j2v!$*d13dzp5&y1rg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',  # vs code preview
     'localhost',  # listen for stripe webhooks
+    'crochet-corner-pp5-75468ce941b2.herokuapp.com', # heroku application
 ]
 
 
@@ -121,12 +123,17 @@ WSGI_APPLICATION = 'crochet_corner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'DATBASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
