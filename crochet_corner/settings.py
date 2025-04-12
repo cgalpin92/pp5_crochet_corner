3e5e9d7436d13c090f33bbb1b3baee98d5bfe87e
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-12 14:37:50 +0100

    fix: amend typos for DATABASE_URLs to resolve errors in heroku deployed sites

--- crochet_corner/settings.py
+++ crochet_corner/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'crochet_corner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATBASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
