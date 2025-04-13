a09b1c9459f01f0edfc636daea2e98a96984b73e
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-13 20:45:50 +0100

    feat: add newlsetter.html page, newsletter view and newsletter url, import newsletter form from mailchip and style newsletter

--- home/views.py
+++ home/views.py
@@ -7,4 +7,17 @@ def index(request):
     """
     A view to return the index page
     """
-    return render(request, 'home/index.html')
+    return render(
+        request, 
+        'home/index.html'
+    )
+
+
+def newsletter(request):
+    """
+    A view to display the newlsetter signup form
+    """
+    return render(
+        request,
+        'home/newsletter.html'
+    )
