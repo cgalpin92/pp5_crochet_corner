a5fd07acd27c00f2c6d9161fd881bcdfef7bf4d7
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-04-15 01:35:25 +0100

    feat: create custom 404 page

---
+++ crochet_corner/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """
+    Error Handler 404 - Page Not Found
+
+    Taken from the Boutique Ado walkthrough
+    """
+    return render(
+        request,
+        "errors/404.html",
+        status=404
+    )
